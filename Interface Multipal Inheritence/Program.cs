90675a5b4d22bc0f36748cd6b9d08e62343e1afd
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 01:46:20 +0530

    Add project files.

---
+++ Interface Multipal Inheritence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Interface_Multipal_Inheritence
+{
+    interface A1
+    {
+       
+        void M1();
+    }
+    interface A2
+    {
+        void M2();
+    }
+    class Program:A1,A2
+    {
+        public void M1()
+        {
+            Console.WriteLine("M1 Method :");
+        }
+        public void M2()
+        {
+            Console.WriteLine("M2 Method :");
+        }
+        static void Main(string[] args)
+        {
+            A1 obj = new Program();
+            obj.M1();
+            A2 obj1 = new Program();
+            obj1.M2();
+        
+            Console.WriteLine(obj.GetHashCode());
+        }
+    }
+}
